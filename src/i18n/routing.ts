00f554a4a510f662b645e5056488dde2608f2bd1
jaceleedev <jaceleedev@gmail.com>
2025-07-08 10:07:21 +0900

    feat(i18n): 일본어 지원 추가
    - 일본어 번역 파일(messages/ja.json) 추가
    - i18n 라우팅 설정에 'ja' 로케일 등록
    - 일관성을 위해 영문 전화번호 형식 수정

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["ko", "en"],
+  locales: ["ko", "en", "ja"],
 
   // Used when no locale matches
   defaultLocale: "ko",
